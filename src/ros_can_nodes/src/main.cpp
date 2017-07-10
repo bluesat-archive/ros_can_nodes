6698a4e35f46b08e7387453770beeaebd4c04111
nunodasneves <ndasneves@gmail.com>
2017-07-10 12:42:13 +1000

    ROC-4 added everything from roscpp. should be easy to change singletons

--- src/ros_can_nodes/src/main.cpp
+++ src/ros_can_nodes/src/main.cpp
@@ -1,9 +1,26 @@
-#include "xmlrpc_manager.h"
+#include "ros/xmlrpc_manager.h"
+#include <iostream>
+
+using namespace std;
 
 namespace ros {
 
     void myTest() {
-        XMLRPCManagerPtr p = XMLRPCManager::instance();
+        XMLRPCManagerPtr srvr1(new XMLRPCManager);
+        XMLRPCManagerPtr srvr2(new XMLRPCManager);
+        XMLRPCManagerPtr srvr3(new XMLRPCManager);
+        srvr1->start();
+        srvr2->start();
+        srvr3->start();
+        cout << srvr1->getServerURI() << endl;
+        cout << srvr2->getServerURI() << endl;
+        cout << srvr3->getServerURI() << endl;
+        cout << srvr1->getServerURI() << endl;
+        srvr1->shutdown();
+        cout << srvr2->getServerURI() << endl;
+        srvr2->shutdown();
+        cout << srvr3->getServerURI() << endl;
+        srvr3->shutdown();
     }
 
 }
