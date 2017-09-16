ba1c7465042d9fbd6626f28630841e4b6e52aabd
nunodasneves <ndasneves@gmail.com>
2017-09-16 11:52:06 +1000

    ROC-4 fixed binding issue in topic manager

--- src/ros_can_nodes/src/main.cpp
+++ src/ros_can_nodes/src/main.cpp
@@ -7,18 +7,29 @@ namespace roscan
     RosCanNode::RosCanNode(std::string name) {
         name_ = name;
 
+        std::cout << "  Creating xmlrpc manager\n";
         xmlrpcManager.reset(new XMLRPCManager);
-        xmlrpcManager->start();
 
+        std::cout << "  Creating poll manager\n";
         pollManager.reset(new PollManager);
+        std::cout << "  Starting poll manager\n";
         pollManager->start();
 
+        std::cout << "  Creating connection manager\n";
         connectionManager.reset(new ConnectionManager);
+        std::cout << "  Starting connection manager\n";
         connectionManager->start(*this);
 
+        std::cout << "  Creating topic manager\n";
         RosCanNodePtr nodeptr = boost::make_shared<RosCanNode>(*this);
         topicManager.reset(new TopicManager);
+        std::cout << "  Starting topic manager\n";
         topicManager->start(nodeptr);
+        std::cout << "  Done!\n";
+
+        // xmlrpc manager must be started _after_ all functions are bound to it
+        std::cout << "  Starting xmlrpc manager\n";
+        xmlrpcManager->start();
 
     }
 
@@ -33,11 +44,13 @@ int main() {
 
     network::init();
 
+    std::cout << "Creating new node\n";
     RosCanNode *node = new RosCanNode("bob");
     std::cout << node->xmlrpcManager->getServerURI() << std::endl;
 
     // mini shell for testing lel
     while (1) {
+        std::cout << "$ ";
         std::string s;
         std::cin >> s;
         if (s == "exit") break;
