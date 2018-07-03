84a9cca0d9ad81b39175b54f0d043dd3faab8d6b
simonireland <spir109@cse.unsw.edu.au>
2018-07-03 21:18:36 +1000

    ROC-11 fixes

--- src/ros_can_nodes/include/RosCanNode.hpp
+++ src/ros_can_nodes/include/RosCanNode.hpp
@@ -23,7 +23,7 @@
 #include "ros_node_lib/callback_queue.h"
 #include <std_msgs/String.h>
 #include <bitset>
-#include "ros_type_introspection/ros_introspection.hpp"
+#include <ros_type_introspection/ros_introspection.hpp>
 
 #define MAX_NODES 16
 #define MAX_TOPICS 128
