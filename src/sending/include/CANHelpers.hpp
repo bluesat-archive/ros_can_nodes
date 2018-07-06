119aa36f1061d3d55f44c4539d61a947538df5db
YiweiHan <yiweih@live.com>
2018-07-06 22:02:10 +1000

    ROC-10 #time 1h add can sending code

---
+++ src/sending/include/CANHelpers.hpp
@@ -0,0 +1,25 @@
+/*
+ * Date Started: 3/12/2017
+ * Original Author: Simon Ireland
+ * Editors:
+ * ROS Node Name:
+ * ROS Package: ros_can_nodes
+ * Purpose:
+ * This code is released under the MIT  License. Copyright BLUEsat UNSW, 2017
+ */
+
+#ifndef CANHELPERS_H
+#define CANHELPERS_H
+
+#include <cstdint>
+
+class CANHelpers{
+    public:
+        static int open_can_port(const char *port);
+
+        static int send_can_port(struct can_frame *frame);
+
+        static void close_can_port();
+};
+
+#endif // CANHELPERS_H
--- src/sending/src/MessageBuffer.cpp
+++ src/sending/src/MessageBuffer.cpp
@@ -4,6 +4,7 @@
 #include <mutex>
 #include <condition_variable>
 #include <thread>
+#include "CANHelpers.hpp"
 
 void MessageBuffer::push(const struct can_frame& data) {
     std::unique_lock<std::mutex> lk(mutex);
@@ -11,7 +12,7 @@ void MessageBuffer::push(const struct can_frame& data) {
     q.push(data);
     
     lk.unlock();
-    cv.notify_all();
+    cv.notify_one();
 }
 
 struct can_frame MessageBuffer::pop() {
@@ -27,22 +28,35 @@ struct can_frame MessageBuffer::pop() {
 
 void MessageBuffer::startSendThread() {
     std::thread sender{[this]() {
+        auto can_port = "can0";
+
         while (true) {
             auto data = pop();
-            //std::cout << "sending " << data << std::endl;
+            
+            // debug exit condition
+            if (data.__res0 == 8) {
+                std::cout << "exit condition\n";
+                break;
+            }
+
             std::cout << data.can_id << ":";
             for (int i = 0;i < data.can_dlc;++i) {
                 std::cout << data.data[i];
             }
             std::cout << "\n";
 
-            // TODO send code here
-
-            // debug exit condition
-            if (data.__res0 == 8) {
-                std::cout << "exit condition\n";
-                break;
+            if (CANHelpers::open_can_port(can_port) < 0) {
+                std::cerr << "send failed: can port \"" << can_port << "\" open failed\n";
+                continue;
             }
+
+            if (CANHelpers::send_can_port(&data) < 0) {
+                std::cerr << "send failed: frame could not be sent\n";
+                CANHelpers::close_can_port();
+                continue;
+            }
+
+            CANHelpers::close_can_port();
         }
     }};
     sender.detach();
