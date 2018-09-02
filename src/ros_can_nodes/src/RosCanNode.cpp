5796dd3301df0016db6524e1c98a01db37d78a11
BLUEsat <info@bluesat.com.au>
2018-09-02 18:04:03 +1000

    ROC-14 arm additions

--- src/ros_can_nodes/src/RosCanNode.cpp
+++ src/ros_can_nodes/src/RosCanNode.cpp
@@ -271,7 +271,7 @@ namespace roscan {
         } else if (id_ == 0 ){
             header |= ((topicID * 2 + 1) << ROSCANConstants::ROSTopic::bitshift_topic_id);
 	} else if (id_ == 4) {
-            header |= ((topicID) << ROSCANConstants::ROSTopic::bitshift_topic_id);
+            header |= ((topicID + 8) << ROSCANConstants::ROSTopic::bitshift_topic_id);
         }
 	header |= (id_ << ROSCANConstants::ROSTopic::bitshift_nid);
         header |= (0 << ROSCANConstants::ROSTopic::bitshift_msg_num);
